f614e97c4679b15f4a954e807363176f42acfa6c
Kakadu <Kakadu@pm.me>
2023-03-07 01:19:57 +0300

    SourceS
    Signed-off-by: Kakadu <Kakadu@pm.me>

--- papers/lambda2023/cpp/lambda.h
+++ papers/lambda2023/cpp/lambda.h
@@ -1,15 +1,16 @@
 #pragma once
+
 #include <ostream>
 #include "doctest.h"
-#include <iostream>
 
 struct ulc;
 struct ulc* var(char* name);
 struct ulc* app(struct ulc *f, struct ulc *arg);
 struct ulc* abs(char* name, struct ulc *body);
-void pp(std::ostream& out, struct ulc *root);
 
 struct Strategy;
 struct ulc* applyStrategy(Strategy *s, struct ulc *root);
 extern struct Strategy CallByValue;
+
 void trace(struct ulc* root);
+void pp(std::ostream& out, struct ulc *root);
